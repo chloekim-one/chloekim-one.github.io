5faf3e23e75501e525caed9cc3c13644dffde3c1
chloekim-one <ckimago26@gmail.com>
2025-05-12 17:01:45 +0000

    Deploying to gh-pages from @ chloekim-one/chloekim-one.github.io@71423beff7290c6c25254ba9f7399e80fff30879 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+  },{id: "nav-resume",
+          title: "resume",
+          description: "My CV",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/resume/";
           },
         },{id: "nav-researches",
           title: "researches",
