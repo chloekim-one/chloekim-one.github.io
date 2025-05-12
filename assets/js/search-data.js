db3c5b5a9ba89a2a81bf059ac2ca4bcef508cb52
chloekim-one <ckimago26@gmail.com>
2025-05-12 17:01:23 +0000

    Deploying to gh-pages from @ chloekim-one/chloekim-one.github.io@8e2ea7a885c2361d9eb69f3842c1edef4910221b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-cv",
+  },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-researches",
+          title: "researches",
+          description: "A growing collection of your cool researches.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/researches/";
           },
         },{id: "nav-extracurricular",
           title: "extracurricular",
