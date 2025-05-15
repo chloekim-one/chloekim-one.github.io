d7a5ec65a92b4d74d6f8a31c5053800e4799df3e
chloekim-one <ckimago26@gmail.com>
2025-05-15 13:03:38 +0000

    Deploying to gh-pages from @ chloekim-one/chloekim-one.github.io@4f3bc94780c189c3be9db226fdb248eca5bb8791 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,21 +445,6 @@ ninja.data = [{
           description: "Network Analysis of Medical Legal Citation (AI and Graphs)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
             },},{
         id: 'social-email',
         title: 'email',
