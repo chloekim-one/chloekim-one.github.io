607a37ceb7d7155f09a62f00bd3000ba963b7e3b
chloekim-one <ckimago26@gmail.com>
2025-05-15 13:00:21 +0000

    Deploying to gh-pages from @ chloekim-one/chloekim-one.github.io@6a2178139f20ef67318e98b671da515c84017f84 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,8 +430,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-intuition-and-semantic-reasoning-in-ai",
+          title: 'Intuition and Semantic Reasoning in AI',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
