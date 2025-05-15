89cec7667afc0d2d6ea9e55b8deea93603f0ae5c
chloekim-one <ckimago26@gmail.com>
2025-05-15 14:29:29 +0000

    Deploying to gh-pages from @ chloekim-one/chloekim-one.github.io@ff0235103b46e65f7b3937b268b295683556e3ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,16 +433,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-website-born",
+          title: 'Website born!',
           description: "",
           section: "News",},{id: "projects-intuition-and-semantic-reasoning-in-ai",
           title: 'Intuition and Semantic Reasoning in AI',
