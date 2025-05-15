167e80a574f9df12c13b9140e63c4b7898d0216f
chloekim-one <ckimago26@gmail.com>
2025-05-15 15:21:55 +0000

    Deploying to gh-pages from @ chloekim-one/chloekim-one.github.io@b484aafab9f267a2c8e18adb4804dc045e46d07b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,7 @@ ninja.data = [{
           },
         },{id: "nav-travels",
           title: "travels",
-          description: "travels--worldwide",
+          description: "--worldwide",
           section: "Navigation",
           handler: () => {
             window.location.href = "/travel/";
