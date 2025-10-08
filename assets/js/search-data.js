35b7f0cad7cb0755221d8089477d6fd4216abd41
chloekim-one <ckimago26@gmail.com>
2025-10-08 08:45:45 +0000

    Deploying to gh-pages from @ chloekim-one/chloekim-one.github.io@f50c324815da8d3693acfc49f3be6685beea116f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-resume",
           title: "resume",
-          description: "",
+          description: "My CV",
           section: "Navigation",
           handler: () => {
             window.location.href = "/resume/";
